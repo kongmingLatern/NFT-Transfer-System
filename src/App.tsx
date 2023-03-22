42ac627e1a1ef55823c48b9af9b1d2e34a9a10a2
凤之兮原 <1577310596@qq.com>
2023-03-22 22:09:19 +0800

    chore: update README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
-import AccountSelector from './AccountSelector'
-import { SubstrateContextProvider } from './substrate-lib'
-import { useEffect, useRef } from 'react'
-import WebcamFaceDetection from './component/common/WebcamFaceDetection'
-import Board from '@/component/paint'
-import Home from './pages/Home'
-import Create from './pages/Create'
+import Home from './pages/Home';
 export default function App() {
-  return (
-    <>
-      {/* <SubstrateContextProvider>
-        <AccountSelector />
-      </SubstrateContextProvider> */}
-      {/* <WebcamFaceDetection /> */}
-      {/* <Home /> */}
-      {/* <Create /> */}
-      {/* <Board/> */}
-    </>
-  )
+	return <Home />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import ReactDOM from 'react-dom/client'
-import '@/assets/index.css'
-import '@/assets/global.css'
-import '@/component/paint/i18n'
+import { ChakraProvider } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import ReactDOM from 'react-dom/client';
+import '@/assets/index.css';
+import '@/assets/global.css';
+import '@/component/paint/i18n';
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
-  <ChakraProvider>
-    <RouterProvider router={router} />
-  </ChakraProvider>
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<ChakraProvider>
+		<RouterProvider router={router} />
+	</ChakraProvider>
+);
